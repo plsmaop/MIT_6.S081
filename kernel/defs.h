2147c256e2e4a3d73904e404c5f5cc950b486531
plsmaop <allenivan@gmail.com>
2020-10-26 19:16:34 +0800

    def for kalloc

--- kernel/defs.h
+++ kernel/defs.h
@@ -63,6 +63,7 @@ void            ramdiskrw(struct buf*);
 void*           kalloc(void);
 void            kfree(void *);
 void            kinit(void);
+void            increaserefcount(uint64);
 
 // log.c
 void            initlog(int, struct superblock*);
