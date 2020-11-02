f2b251968054f196f8f76687361030467f1c1dab
plsmaop <allenivan@gmail.com>
2020-11-02 19:44:01 +0800

    all tests pass

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -30,7 +30,24 @@ barrier()
   // Block until all threads have called barrier() and
   // then increment bstate.round.
   //
-  
+  pthread_mutex_lock(&bstate.barrier_mutex);
+
+  ++bstate.nthread;
+  if (nthread != bstate.nthread)
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+
+  --bstate.nthread;
+  if (bstate.nthread != 0) {
+    pthread_cond_signal(&bstate.barrier_cond);
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+    pthread_mutex_unlock(&bstate.barrier_mutex);
+    return;
+  }
+
+  ++bstate.round;
+  bstate.nthread = 0;
+  pthread_cond_broadcast(&bstate.barrier_cond);
+  pthread_mutex_unlock(&bstate.barrier_mutex);
 }
 
 static void *
